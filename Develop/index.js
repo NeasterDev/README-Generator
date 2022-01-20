1bc89c44eb223fdf6bc8323ec5fc8efd0f95a59f
Nicholas Easter <nicholas.easter17@gmail.com>
2022-01-20 14:22:46 -0800

    added basic inquirer questions and started the layour for the readme

--- Develop/index.js
+++ Develop/index.js
@@ -1,13 +1,93 @@
 // TODO: Include packages needed for this application
+const inquirer = require('inquirer');
+const fs = require('fs');
+
+const gm = require('./utils/generateMarkdown.js');
+const { file } = require('tmp');
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = ['Project title:', 'Description:', 'Installation', 'Usage:', 'Contributing:', 'Tests:', 'License:', 'Github Username:', 'Email:'];
+const readMe = './testreadme.md';
+
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+    console.log("Created new file.");
+    fs.writeFile(fileName, data, err => {
+        if (err) {
+            console.error(err);
+            return;
+        };
+    });
+};
 
 // TODO: Create a function to initialize app
 function init() {
+    inquirer.prompt([
+        /* Pass your questions in here */
+        {
+            type: 'input',
+            name: 'question1',
+            message: questions[0]
+        },
+        {
+            type: 'input',
+            name: 'question2',
+            message: questions[1]
+        },
+        {
+            type: 'input',
+            name: 'question3',
+            message: questions[2]
+        },
+        {
+            type: 'input',
+            name: 'question4',
+            message: questions[3]
+        },
+        {
+            type: 'input',
+            name: 'question5',
+            message: questions[4]
+        },
+        {
+            type: 'input',
+            name: 'question6',
+            message: questions[5]
+        },
+        {
+            type: 'input',
+            name: 'question7',
+            message: questions[6]
+        },
+        {
+            type: 'input',
+            name: 'question8',
+            message: questions[7]
+        },
+        {
+            type: 'input',
+            name: 'question9',
+            message: questions[8]
+        },
+
+    ])
+    .then((answers) => {
+        // Use user feedback for... whatever!!
+        console.log(answers.question1);
+        writeToFile(readMe, `# ${answers.question1}\n\n## Description\n\n${answers.question2}\n\n## Table of contents\n- [Installation](#Installation)\n- ` +
+                    `[Usage](#Usage)\n- Credits\n- License` + `\n## Installation\n\n${answers.question3}\n\n## Usage\n\n${answers.question4}\n\nCredits` + 
+                    `\n${answers.question5}\n\n## License\n${answers.question6}\n\n ## Badges\n${gm.renderLicenseBadge()}\n\n## Questions\n${answers.question7}` +
+                    `\n${answers.question8}`);
+
+    })
+    .catch((error) => {
+        if (error.isTtyError) {
+        // Prompt couldn't be rendered in the current environment
+        } else {
+        // Something else went wrong
+        }
+  });
 
 }
 
---
+++ Develop/testreadme.md
@@ -0,0 +1,30 @@
+# CorCor
+
+## Description
+
+This is for cor
+
+## Table of contents
+- [Installation](#Installation)
+- [Usage](#Usage)
+- Credits
+- License
+## Installation
+
+Install it like cor
+
+## Usage
+
+For cor
+
+Credits
+CorcO
+
+## License
+
+
+ ## Badges
+undefined
+
+## Questions
+
